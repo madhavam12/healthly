1bd4a30220820dc701b90f5d8725ce31fcd782f2
madhavam12 <66830856+madhavam12@users.noreply.github.com>
2021-08-13 18:27:58 +0530

    added the bmi calculator, refacoring the code and upgrading the dependencies

---
+++ healthly/lib/bmiCalculationScreens/app_bar.dart
@@ -0,0 +1,57 @@
+import 'package:healthly/bmiCalculationScreens/input_page/input_page_styles.dart';
+import 'package:healthly/bmiCalculationScreens/widget_utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class BmiAppBar extends StatelessWidget {
+  final bool isInputPage;
+  static const String wavingHandEmoji = "\uD83D\uDC4B";
+  static const String whiteSkinTone = "\uD83C\uDFFB";
+
+  const BmiAppBar({Key key, this.isInputPage = true}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 1.0,
+      child: Container(
+        height: appBarHeight(context),
+        color: Colors.white,
+        child: Padding(
+          padding: EdgeInsets.all(screenAwareSize(16.0, context)),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              _buildLabel(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  RichText _buildLabel(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        style: DefaultTextStyle.of(context)
+            .style
+            .copyWith(fontSize: isInputPage ? 25 : 34.0),
+        children: [
+          TextSpan(
+            text: isInputPage ? "Calculate your BMI" : "Your BMI",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          TextSpan(text: ""),
+        ],
+      ),
+    );
+  }
+
+  // https://github.com/flutter/flutter/issues/9652
+  String getEmoji(BuildContext context) {
+    return Theme.of(context).platform == TargetPlatform.iOS
+        ? wavingHandEmoji
+        : wavingHandEmoji + whiteSkinTone;
+  }
+}
--- healthly/lib/homeScreen/homeScreen.dart
+++ healthly/lib/homeScreen/homeScreen.dart
@@ -13,7 +13,7 @@ import 'package:healthly/recipePages/search_screen.dart';
 import 'package:healthly/profileCreation/docProfileCreation.dart';
 import 'package:healthly/covidDsNew/providers/home_provider.dart';
 import 'package:provider/provider.dart';
-
+import 'package:healthly/bmiCalculationScreens/input_page/input_page.dart';
 import 'package:shrink_sidemenu/shrink_sidemenu.dart';
 import 'package:liquid_ui/liquid_ui.dart';
 import 'package:healthly/Models/userIdModel.dart';
@@ -616,6 +616,124 @@ class _HomeScreenState extends State<HomeScreen> {
                                         ),
                                       ],
                                     ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 50),
+                        Container(
+                          height: 180,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.7),
+                                blurRadius: 25.0,
+                              ),
+                            ],
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            ),
+                            // image: DecorationImage(
+                            //   image: AssetImage('assets/images/covid.png'),
+                            // ),
+                            color: Colors.orange.withOpacity(0.89),
+                          ),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(15),
+                              ),
+                            ),
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Container(
+                                child: Center(
+                                  child: Row(
+                                    children: [
+                                      Align(
+                                        alignment: Alignment.centerLeft,
+                                        child: Container(
+                                          height: 100,
+                                          margin: EdgeInsets.only(
+                                              bottom: 60,
+                                              top: 1,
+                                              right: 5,
+                                              left: 5),
+                                          width: 100,
+                                          decoration: BoxDecoration(
+                                            image: DecorationImage(
+                                              image: AssetImage(
+                                                  "assets/images/food.png"),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      Flexible(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              "Calculate BMI",
+                                              style: TextStyle(
+                                                letterSpacing: 1.5,
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold,
+                                                fontFamily: "QuickSand",
+                                                fontSize: 18.0,
+                                              ),
+                                            ),
+                                            Text(
+                                              "Tap below to calculate your BMI and check if you're healthy. Always stay healthy!",
+                                              maxLines: 4,
+                                              overflow: TextOverflow.ellipsis,
+                                              style: TextStyle(
+                                                color: Colors.white
+                                                    .withOpacity(0.9),
+                                                fontWeight: FontWeight.bold,
+                                                fontFamily: "QuickSand",
+                                                fontSize: 11.0,
+                                              ),
+                                            ),
+                                            SizedBox(height: 10),
+                                            FlatButton.icon(
+                                              onPressed: () async {
+ Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => InputPage()));
+                                              },
+                                              color: Colors.white,
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(
+                                                        30.0),
+                                              ),
+                                              icon: const Icon(
+                                                Icons.search,
+                                                color: Colors.black,
+                                              ),
+                                              label: Text(
+                                                'Find Now!',
+                                                style: Styles.buttonTextStyle
+                                                    .copyWith(
+                                                        color: Colors.black,
+                                                        fontSize: 15,
+                                                        fontFamily:
+                                                            "QuickSand"),
+                                              ),
+                                              textColor: Colors.white,
+                                            ),
+                                            SizedBox(height: 10),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ),
                               ),
